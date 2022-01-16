d37ec35ded007e70dc7e03e9475a6ebd8559d7d6
soulrest <andrew.shop4@gmail.com>
2022-01-16 16:18:22 +0000

    Silenced added user authorization

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import "./App.css";
+import { useState } from "react";
 
+import "./App.css";
+import FirebaseAuthService from "./FirebaseAuthService";
+import LoginForm from "./components/LoginForm";
 // eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
+        <LoginForm existingUser={user}></LoginForm>
       </div>
     </div>
   );
-}
+};
 
 export default App;
